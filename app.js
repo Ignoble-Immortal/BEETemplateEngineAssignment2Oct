222443629c8025e1fc43c3e19a60ba4e1ffd834d
Ojas Gupta <ojasgpt009@gmail.com>
2024-10-23 10:24:06 +0530

    Added product page using partials

--- app.js
+++ app.js
@@ -42,6 +42,71 @@
 //     else console.log(`Listening to Port ${PORT}`);
 // });
 
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const PORT = 8080;
+// app.set('view engine', 'ejs');
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// let tasks = [];
+// app.get("/", (req, res) => {
+//     let name = "Sam";
+//     let place = "Jakhu";
+//     let currentHour = new Date().getHours();
+//     let greeting;
+
+//     if (currentHour < 12) {
+//         greeting = "Good Morning";
+//     } else if (currentHour < 18) {
+//         greeting = "Good Afternoon";
+//     } else {
+//         greeting = "Good Evening";
+//     }
+//     res.render("index", { name, destination: place, greeting, tasks });
+// });
+// app.post("/add-task", (req, res) => {
+//     const newTask = req.body.task;
+//     if (newTask) {
+//         tasks.push(newTask);
+//     }
+//     res.redirect("/todo");
+// });
+// app.post("/edit-task/:id", (req, res) => {
+//     const taskId = req.params.id;
+//     const updatedTask = req.body.updatedTask;
+//     if (updatedTask && tasks[taskId]) {
+//         tasks[taskId] = updatedTask;
+//     }
+//     res.redirect("/todo");
+// });
+// app.post("/delete-task/:id", (req, res) => {
+//     const taskId = req.params.id;
+//     tasks.splice(taskId, 1);
+//     res.redirect("/todo");
+// });
+// app.get("/todo", (req, res) => {
+//     let name = "Sam";
+//     let place = "Jakhu";
+//     let currentHour = new Date().getHours();
+//     let greeting;
+
+//     if (currentHour < 12) {
+//         greeting = "Good Morning";
+//     } else if (currentHour < 18) {
+//         greeting = "Good Afternoon";
+//     } else {
+//         greeting = "Good Evening";
+//     }
+
+//     res.render("index", { name, destination: place, greeting, tasks });
+// });
+
+// app.listen(PORT, (err) => {
+//     if (err) console.log(err);
+//     else console.log(`Listening to Port ${PORT}`);
+// });
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -50,12 +115,16 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 let tasks = [];
+let products = [
+    { name: 'asd', price: 10 },
+    { name: 'wasd', price: 20 },
+    { name: 'qwerty', price: 30 },
+];
 app.get("/", (req, res) => {
     let name = "Sam";
     let place = "Jakhu";
     let currentHour = new Date().getHours();
     let greeting;
-
     if (currentHour < 12) {
         greeting = "Good Morning";
     } else if (currentHour < 18) {
@@ -70,7 +139,7 @@ app.post("/add-task", (req, res) => {
     if (newTask) {
         tasks.push(newTask);
     }
-    res.redirect("/todo");
+    res.redirect("/");
 });
 app.post("/edit-task/:id", (req, res) => {
     const taskId = req.params.id;
@@ -78,30 +147,34 @@ app.post("/edit-task/:id", (req, res) => {
     if (updatedTask && tasks[taskId]) {
         tasks[taskId] = updatedTask;
     }
-    res.redirect("/todo");
+    res.redirect("/");
 });
 app.post("/delete-task/:id", (req, res) => {
     const taskId = req.params.id;
     tasks.splice(taskId, 1);
-    res.redirect("/todo");
+    res.redirect("/");
 });
-app.get("/todo", (req, res) => {
-    let name = "Sam";
-    let place = "Jakhu";
-    let currentHour = new Date().getHours();
-    let greeting;
-
-    if (currentHour < 12) {
-        greeting = "Good Morning";
-    } else if (currentHour < 18) {
-        greeting = "Good Afternoon";
-    } else {
-        greeting = "Good Evening";
-    }
-
-    res.render("index", { name, destination: place, greeting, tasks });
+// app.get("/todo", (req, res) => {
+//     let name = "Sam";
+//     let place = "Jakhu";
+//     let currentHour = new Date().getHours();
+//     let greeting;
+//     if (currentHour < 12) {
+//         greeting = "Good Morning";
+//     } else if (currentHour < 18) {
+//         greeting = "Good Afternoon";
+//     } else {
+//         greeting = "Good Evening";
+//     }
+//     res.render("index", { name, destination: place, greeting, tasks });
+// });
+app.get("/products", (req, res) => {
+    const searchQuery = req.query.search || "";
+    const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    res.render("products", { products: filteredProducts });
 });
-
 app.listen(PORT, (err) => {
     if (err) console.log(err);
     else console.log(`Listening to Port ${PORT}`);
